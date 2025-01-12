353e33997df44284a772cd37802e5fe6e94548b8
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-12 22:03:15 +0700

    menambahkan sealed class FormState

--- app/src/main/java/com/example/mvvm_firebase/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/mvvm_firebase/ui/home/viewmodel/InsertViewModel.kt
@@ -11,6 +11,12 @@ class InsertViewModel(
 
 
 
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
 
 data class InsertUiState(
     val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
