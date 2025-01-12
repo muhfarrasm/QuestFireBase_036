52478cf98b2d99991505dda7213c6b20ec5bf115
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-12 23:16:16 +0700

    memperbarui Interface DestinasiNavigasi

--- app/src/main/java/com/example/mvvm_firebase/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/mvvm_firebase/ui/navigation/DestinasiNavigasi.kt
@@ -6,13 +6,13 @@ interface DestinasiNavigasi {
 }
 
 object DestinasiHome : DestinasiNavigasi {
-    override val route: String
-    override val titleRes: String
+    override val route: String = "home"
+    override val titleRes: String = "home"
 }
 
 object DestinasiInsert : DestinasiNavigasi {
-    override val route: String
-    override val titleRes: String
+    override val route: String = "insert"
+    override val titleRes: String = "insert"
 
 }
 
