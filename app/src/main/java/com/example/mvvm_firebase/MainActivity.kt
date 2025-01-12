9f622016fcc1186ea63d89d2289b5abf3c688d41
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-12 23:23:39 +0700

    Memanggil Pengelola Halaman ke main activity

--- app/src/main/java/com/example/mvvm_firebase/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_firebase/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mvvm_firebase
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,19 +13,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mvvm_firebase.ui.navigation.PengelolaHalaman
 import com.example.mvvm_firebase.ui.pages.HomeScreen
 import com.example.mvvm_firebase.ui.theme.MVVM_FirebaseTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MVVM_FirebaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(
-
-                        modifier = Modifier.padding(innerPadding), navigateToltemEntry = {}
+                    PengelolaHalaman(
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
