228660f2cf81c52305b630a9b662a470260a2f6e
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-12 23:01:57 +0700

    Menambahkan Interface DestinasiNavigasi, untuk navigasi antar halaman

---
+++ app/src/main/java/com/example/mvvm_firebase/ui/navigation/DestinasiNavigasi.kt
@@ -0,0 +1,18 @@
+package com.example.mvvm_firebase.ui.navigation
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String
+    override val titleRes: String
+}
+
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String
+    override val titleRes: String
+
+}
+
