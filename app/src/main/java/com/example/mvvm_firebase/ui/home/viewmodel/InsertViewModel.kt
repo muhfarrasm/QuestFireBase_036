0a0730387127a645ea129eb8ade9810a97c9245b
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-12 22:00:49 +0700

    menambahkan data class MahasiswaEvent

--- app/src/main/java/com/example/mvvm_firebase/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/mvvm_firebase/ui/home/viewmodel/InsertViewModel.kt
@@ -6,3 +6,14 @@ import com.example.mvvm_firebase.repository.RepositoryMhs
 class InsertViewModel(
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel() {}
+
+
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
