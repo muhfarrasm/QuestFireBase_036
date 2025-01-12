5d4c3cdcd201f91c38aea0d41c1242d76f998171
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-12 21:59:27 +0700

    menambahkan class InsertViewModel

---
+++ app/src/main/java/com/example/mvvm_firebase/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.mvvm_firebase.ui.home.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.mvvm_firebase.repository.RepositoryMhs
+
+class InsertViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {}
