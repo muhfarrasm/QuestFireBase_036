78cc414f870e1bf19d0201d947e866892e922e7f
muhfarrasm <muhammadfarrasm@gmail.com>
2025-01-12 22:09:45 +0700

    menambahkan initializer untuk memanggil InsertViewModel

--- app/src/main/java/com/example/mvvm_firebase/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mvvm_firebase/ui/PenyediaViewModel.kt
@@ -10,11 +10,13 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.mvvm_firebase.MahasiswaApp
 import com.example.mvvm_firebase.di.MahasiswaContainer
 import com.example.mvvm_firebase.ui.home.viewmodel.HomeViewModel
+import com.example.mvvm_firebase.ui.home.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     val Factory = viewModelFactory {
         initializer { HomeViewModel(mahasiswaApp().container.repositoryMhs) }
+        initializer { InsertViewModel(mahasiswaApp().container.repositoryMhs) }
 
     }
 }
